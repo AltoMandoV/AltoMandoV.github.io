9c4c98cc65b8129969636ce4d5c1659dd12583ae
Javier Geli Megías <javiergelimegias@MBP-de-Javier.lan>
2021-01-07 02:14:15 +0100

    Team builder in process

---
+++ teamBuilder.js
@@ -0,0 +1,21 @@
+/*
+Para cada Pokémon:
+-Nombre
+-Nivel
+-Naturaleza
+-Ataque 1
+-Ataque 2
+-Ataque 3
+-Ataque 4
+-Objeto
+-IVs
+-EVs
+-Gender
+-Shiny
+-Nickname
+--> No ataques repetidos
+--> Cumplir con recuento de IVs y EVs
+
+Generales
+--> No objetos repetidos
+*/
